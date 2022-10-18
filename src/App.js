87f1a5e5f3fab46371128d954c79c270f093cc4d
Otis <ocparsons@gmail.com>
2022-10-18 17:24:50 +0100

    Basic Expense Tracker

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
 import './App.css';
+import { Header } from './components/header';
+import { Balance} from './components/balance';
+import { IncomeExpenses } from './components/IncomeExpenses';
+import { TransactionList } from './components/TransactionList';
+import { AddTransaction} from './components/AddTransaction';
+
+import { GlobalProvider } from './context/globalState';
 
 function App() {
   return (
-    <div>
-    Hello world
-    </div>
+    <GlobalProvider>
+      <Header />
+      <div className="container">
+        <Balance />
+        <IncomeExpenses />
+        <TransactionList />
+        <AddTransaction />
+      </div>
+    </GlobalProvider>
   );
 }
 
